9219b4bb12bbc0ad204bf6ffe07de61e97677373
Timur Khanafin <mindiisky@ya.ru>
2024-12-30 13:14:45 +0300

    Collections binarySearch

--- src/main/java/org/example/MyCollections.java
+++ src/main/java/org/example/MyCollections.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class MyCollections {
+    public static <T extends Comparable<? super T>> int binarySearch(List<T> list, T key) {
+        return Collections.binarySearch(list, key);
+    }
+
+    public static <T> int binarySearch(List<? extends T> list, T key, Comparator<? super T> c) {
+        return Collections.binarySearch(list, key, c);
+    }
 }
